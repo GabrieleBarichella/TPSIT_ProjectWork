86b24b73142b7a0e8ec23cb4ce0efe79ddee8e5c
heybarryy <gabrielebarichella0@gmail.com>
2025-05-11 19:41:44 +0200

    dichiarazione metodi classe serra

--- include/greenhouse.h
+++ include/greenhouse.h
@@ -10,17 +10,16 @@
 class Greenhouse {
 private:
     Clock clock;
-public:
     std::list<Implant*> implants;
-
+public:
     void add_implant(Implant* implant);
     void remove_implant(Implant* implant);
 
-    void set_implant_on(std::string n);
-    void set_implant_off(std::string n);
-    void set_implant_timers(std::string start, std::string stop);
+    void set_implant_on(std::string name);
+    void set_implant_off(std::string name);
+    void set_implant_timers(std::string name, std::string start, std::string stop);
     void show();
-    void set_time(std::string t);
+    void set_time(std::string time);
     void reset_time();
     void reset_timers();
     void reset_all();
